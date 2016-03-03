fd71a1921484dd17f5150a195e4aaa620ca67c59
thetbsi <maytt4b567@gmail.com>
2016-03-02 20:46:09 -0500

    save

--- go/src/eightball/main.go
+++ go/src/eightball/main.go
@@ -4,3 +4,12 @@ import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
 )
+
+var answer = []string{
+	"Yes",
+	"No",
+	"Maybe",
+	"Recalculating",
+	"Shut down initiated, Reboot in process",
+	"He needs some milk (put in by retep)"
+
